d04ff2e2d45e145f4b3849044912c187df0b0690
charliecan2 <carloscq920@gmail.com>
2021-01-23 13:58:14 -0600

    Created a series of prompts using inquirer

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
-inquirer.prompt(
-    [
+inquirer.prompt([
     {
-        type: "input"
-        
+        type: "input",
+        message: "Please write your project's name",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "What is the description of your product?",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "How would you like for your project/applicaiton to be used?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "Who are the contributors of this project?",
+        name: "contributors"
+    },
+    {
+        type: "checkbox",
+        message: "Please select a license",
+        choices: [
+            "Apache",
+            "MIT",
+            "ISC",
+            "GNU GPLv3"
+        ],
+        name: "license"
+    },
+    {
+        type: "input",
+        message: "What is your Github username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
     }
 ]);
